90fd063c58aaa94e6b5aad8b758ec0fdabf5b3cf
Maryam <m.moh@gmx.ch>
2021-03-02 14:17:22 +0100

    BIG O NOTATION used to describe the PERFORMANCE OF AN ALGORITHM

--- src/com/MORTGAGE/Main.java
+++ src/com/MORTGAGE/Main.java
@@ -3,6 +3,41 @@ package com.MORTGAGE;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// before we talk about Algorithm & Data Structure we need to learn the BIG O NOTATION. we USE it to describe
+    // the PERFORMANCE OF AN ALGORITHM.
+
+    /*
+    WIKI:
+    Big O notation is a mathematical notation that describes
+    the limiting behavior of a function when the argument tends towards a particular value or infinity.
+    In computer science, big O notation is used to classify algorithms according to
+    how their run time or space requirements grow as the input size grows.
+     */
+     /*
+     the underlying concepts are not that hard:
+     we USE BIG O NOTATION to describe the PERFORMANCE OF AN ALGORITHM. it helps us to know if a given ALGORITHM
+     is scalable or not. that is: is this ALGORITHM going to scale well as the INPUT grows really LARGE????
+     so
+     just because your code executes quickly does not mean it is going
+     to perform well when you give it a LARGE DATA SET. that is why we USE O(N)
+     to describe the performance of an Algorithm
+
+     what does this have to do with DATA STRUCTURE AND ALGORITHM?
+     we will learn that certain OPERATION can be less or more costly depending on what DATA STRUCTURE WE USE.
+     eg: accessing an ARRAY ELEMENT BY ITS INDEX is super fast. BUT
+     ARRAYS HAVE a FIXED LENGTH AND IF YOU WANNA CONSTANTLY ADD OR REMOVE items from them
+     they have to get resized. and it will get costly as the size of our INPUT GROWS LARGE.
+
+     so
+     if that is what we need to do: THEN WE HAVE TO USE ANOTHER DATA STRUCTURE CALLED: a LINKED LIST
+     these data structures can grow or shrink vey quickly BUT accessing a LINKED LIST ELEMENT BY ITS INDEX
+     is slow.
+
+     THAT IS WHY YOU NEED TO LEARN ABOUT THE BIG O NOTATION FIRST before we talk about the various DATA STRUCTURES
+
+     in JOB INTERVIEWS the ask you about BIG O and DATA STRUCTURE OR ALGORITHM.
+      */
+
+
     }
 }
