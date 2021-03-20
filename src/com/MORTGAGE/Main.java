dedf3f62ee832b2bbb7431b0f5de06a0d8252f5d
mttbas <m.moh@gmx.ch>
2021-03-20 19:56:56 +0100

    LinkedLists in Java and how they work....

--- src/com/MORTGAGE/Main.java
+++ src/com/MORTGAGE/Main.java
@@ -2,40 +2,32 @@ package com.MORTGAGE;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        // JAVA has 2 implementation of dynamic Arrays:
-        // CLASS 1: Vector<E>    (java.util) - will grow by 100% of its size when it gets FULL.
-        //                                   - all the methods in Vector class are Synchronized
-        //                                   - only a SINGLE THREAD can access that METHOD
-        // CLASS 2: ArrayList<E> (java.util) - will grow by 50% of its size when it gets FULL.
-        //                                   - the methods are NOT Synchronized
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(20);
-        list.add(60);
-        list.add(20);
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
         System.out.println(list);
-        System.out.println(list.indexOf(30));
-        System.out.println(list.lastIndexOf(20));
-        list.remove(1);
+        list.addFirst(1002);
+        list.addFirst(120);
+        list.addFirst(5);
         System.out.println(list);
-        System.out.println(list.indexOf(30));
-        System.out.println(list.lastIndexOf(20));
-        System.out.println(list.contains(100));
+        list.remove(0);
+        System.out.println(list);
+        System.out.println(list.contains(1002));
+        System.out.println(list.indexOf(1002));
+        list.removeLast();
+        list.removeFirst();
+        System.out.println(list);
         System.out.println(list.size());
-        list.toArray(); // this will convert this list into a regular array OBJECT
-        // there are times when you wanna work with a regular ARRAY OBJECT
-        // lets say you have a METHOD that accepts ONLY AN ARRAY and you cannot pass
-        // an ARRAYLIST CLASS THERE. in this case we can easily convert you ARRAYLIST into
-        // regular Array Object.
-
-
-
+        var array = list.toArray();
+        // in times you wanna work with array you can convert a Linked List in Array
+        // now we can USE ARRAY CLASS to convert this Array into String
+        System.out.println(Arrays.toString(array));
     }
 
 }
